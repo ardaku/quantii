e63147c7b922c9b1d20143c5db01ca1a741d853a
Jeron Aldaron Lau <aldaronlau@gmail.com>
2021-12-12 01:15:34 -0600

    Something that doesn't compile.

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,34 @@
 
 #[macro_use] extern crate novuskinc;
 
-pub fn arc_setup() {
+use novuskinc::kernel::syscalls;
 
+const APP_EXE: &[u8] = include_bytes!("hello_world.wasm");
+
+struct System;
+
+impl ardaku::System for System {
+    fn sleep(&self) -> (ardaku::Event, u32) {
+        let byte = novuskinc::kernel::io::safe_sys_read();
+        (ardaku::Event::Read, byte.into())
+    }
+
+    fn write(&self, line: &[u8]) {
+        for c in line {
+            novuskinc::kernel::io::safe_sys_write(*c);
+        }
+        novuskinc::kernel::io::safe_sys_write(b'\n');
+    }
+
+    fn version(&self) -> u32 {
+        unsafe { syscalls::syscall(syscalls::VERSION, 0) }.into()
+    }
+
+    fn reboot(&self) {
+        unsafe { syscalls::syscall(syscalls::REBOOT, 0) };
+    }
+}
+
+pub fn arc_setup() {
+    ardaku::start(System, APP_EXE).unwrap();
 }
