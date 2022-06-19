25aee9bfff7b753fc06cb4fa4ccab263007d8733
CATboardBETA <catboardbeta@gmail.com>
2022-06-19 16:57:58 -0400

    Add more comments
    Took 1 hour 30 minutes
    
    Took 4 minutes

--- quantii/src/lib.rs
+++ quantii/src/lib.rs
@@ -28,18 +28,27 @@ extern crate compiler_builtins_patch;
 
 use novuskinc::kernel::syscalls;
 
+/// Novusk config
 pub mod novusk;
 
+/// The executable WASM for the kernel's initially running App
 const APP_EXE: &[u8] = include_bytes!("hello_world.wasm");
 
 struct System;
 
 impl ardaku::System for System {
+    /// Sleep until an event interrupt occurs.
     fn sleep(&self) -> (ardaku::Event, u32) {
         let byte = novuskinc::kernel::io::safe_sys_read();
         (ardaku::Event::Read, byte.into())
     }
 
+    /// Write to the console.
+    ///
+    /// Note that the console is not shown to the actual onscreen
+    /// OS (QEMU/emulators, VMs, or real hardware), but rather
+    /// in a separate console. For QEMU, this would be the
+    /// console used to start the QEMU process.
     fn write(&self, line: &[u8]) {
         for c in line {
             novuskinc::kernel::io::safe_sys_write(*c);
@@ -47,6 +56,7 @@ impl ardaku::System for System {
         novuskinc::kernel::io::safe_sys_write(b'\n');
     }
 
+    /// Return version of the kernel.
     fn version(&self) -> u32 {
         #[cfg(not(target_arch = "riscv32"))]
         unsafe {
@@ -55,14 +65,23 @@ impl ardaku::System for System {
 
         #[cfg(target_arch = "riscv32")]
         {
+            // TODO: Implement a better versioning system that works for RISC-V.
             3
         }
     }
 
-    fn reboot(&self) {
+    /// Reboot the system.
+    ///
+    /// This does not work on RISC-V targets.
+    fn reboot(&self){
+        // This is a no-op on RISC-V.
+        #[cfg(target_arch = "riscv32")]
+        Self::write(self, b"Reboot not supported on RISC-V");
+
+        // On all other targets, proceed to reboot.
         #[cfg(not(target_arch = "riscv32"))]
         unsafe {
-            syscalls::syscall(syscalls::REBOOT, 0)
+            syscalls::syscall(syscalls::REBOOT, 0);
         };
     }
 }
@@ -76,6 +95,7 @@ pub fn setup() -> ! {
 
     System.write(b"\n=== ARDAKU STOPPED ===\n");
 
+    // TODO: Do something more proper after the app has stopped.
     loop {
         System.sleep();
     }
