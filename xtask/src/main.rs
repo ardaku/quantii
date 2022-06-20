d968c5fd7bba547381493a398bb8e6db72fc19e0
Jeron Aldaron Lau <aldaronlau@gmail.com>
2022-06-20 17:35:35 -0500

    Fix M1 MacOS Build (#23)
    * Initial attempt
    
    * Fix not working

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -20,7 +20,7 @@
 // Lesser General Public License along with
 // Quantii. If not, see <https://www.gnu.org/licenses/>.
 
-use std::{env, process::Command};
+use std::{env, fs, process::Command};
 
 fn help() {
     println!("Commands:");
@@ -42,8 +42,8 @@ fn build() {
 }
 
 fn dist_riscv() {
-    let _ = std::fs::remove_dir_all("../target/dist/quantii-riscv");
-    let _ = std::fs::create_dir_all("../target/dist/quantii-riscv");
+    let _ = fs::remove_dir_all("../target/dist/quantii-riscv");
+    let _ = fs::create_dir_all("../target/dist/quantii-riscv");
 
     let status = Command::new("cargo")
         .args([
@@ -56,7 +56,7 @@ fn dist_riscv() {
         .expect("failed to execute process");
     assert!(status.success());
 
-    std::fs::copy(
+    fs::copy(
         "../target/riscv32imac-unknown-none-elf/release/quantii",
         "../target/dist/quantii-riscv/quantii.img",
     )
@@ -86,8 +86,8 @@ fn dist_ci() {
 }
 
 fn dist_arm() {
-    let _ = std::fs::remove_dir_all("../target/dist/quantii-arm");
-    let _ = std::fs::create_dir_all("../target/dist/quantii-arm");
+    let _ = fs::remove_dir_all("../target/dist/quantii-arm");
+    let _ = fs::create_dir_all("../target/dist/quantii-arm");
 
     let status = Command::new("cargo")
         .args(["build", "--release", "--target", "aarch64-novusk.json"])
@@ -156,8 +156,17 @@ fn main() {
     let curdir = curdir.as_path().to_str().unwrap();
     let path = env::var("PATH").unwrap();
     env::set_var("PATH", format!("{curdir}/target/bin:{}", path));
-    // Build tools
+    // Install runner
     build();
+    // Create config
+    const CONFIG_DIR: &str = "quantii/.cargo/";
+    const CONFIG_FILE: &str = "quantii/.cargo/config.toml";
+    fs::create_dir_all(CONFIG_DIR).unwrap();
+    if cfg!(target_arch = "aarch64") && cfg!(target_vendor = "apple") {
+        fs::copy("config/macos.toml", CONFIG_FILE).unwrap();
+    } else {
+        fs::copy("config/linux.toml", CONFIG_FILE).unwrap();
+    }
     // Go to Quantii
     env::set_current_dir("quantii").unwrap();
     // Options
