7d3363ce4df115331881289bc9a4e1190ae5a499
CATboardBETA <catboardbeta@gmail.com>
2022-10-10 11:06:23 -0400

    fix all the wierd temp commits 2
    Took 2 minutes

--- quantii/src/main.rs
+++ quantii/src/main.rs
@@ -4,7 +4,6 @@
 #![feature(ptr_metadata)]
 #![feature(slice_take)]
 
-extern crate novusk;
 extern crate quantii;
 // Enable libm
 extern crate externc_libm;
@@ -12,10 +11,6 @@ extern crate alloc;
 
 use alloc::string::ToString;
 use ardaku::System;
-use novusk::drivers::gpu::armfb::{armfb_end, armfb_init, BLACK, graphics_pixel, graphics_write, LIGHT_GRAY, WHITE};
-use novusk::drivers::gpu::armfb::a64::A64Fb;
-use novusk::fb::FbColor;
-use novusk::fb::framebuffer::FrameBufferGraphics;
 use quantii::setup;
 
 
